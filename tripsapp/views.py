0dead000382f853a72f3607909eca9cf7ed73b7a
SlimanovQ8 <Slimanovq8@gmail.com>
2022-08-01 21:26:52 +0300

    s

--- tripsapp/views.py
+++ tripsapp/views.py
@@ -4,9 +4,6 @@ from django.shortcuts import render, HttpResponse
 from rest_framework import generics
 from rest_framework.generics import ListAPIView
 from django.contrib.auth.models import User
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
-from rest_framework_simplejwt.views import TokenObtainPairView
-
 
 
 from .serializers import UserCreateSerializer,ViewTripsSerializer
